5bc5d9c52d18ab181757c9a57c95717d228605f8
Logan Campbell <colin@logaan.net>
2013-07-02 01:39:44 +1000

    displaying search results

--- test/promise_list/quick_search_test.cljs
+++ test/promise_list/quick_search_test.cljs
@@ -1,8 +1,8 @@
 (ns promise-list.quick-search-test
   (:use [jayq.util :only [log]]
-        [jayq.core :only [$ on val text]]
+        [jayq.core :only [$ on val text remove append]]
         [promise-list.pcell :only [deferred]]
-        [promise-list.plist :only [with-open-plist append! mapd* concat*]]))
+        [promise-list.plist :only [with-open-plist append! map* mapd* concat*]]))
 
 (defn summarise [event]
   (let [target (aget event "target")]
@@ -26,11 +26,24 @@
 (defn update-query-title [new-title]
   (text query-title new-title))
 
-(let [changes  (event-list query-input "change")
-      keyups   (event-list query-input "keyup")
-      q-events (concat* changes keyups)
-      queries  (mapd* summarise q-events)
-      qtitles  (mapd* :value queries)]
-  (mapd* update-query-title qtitles)
-  (mapd* transparent-log qtitles))
+(defn update-results-list [results]
+  (remove ($ "#results li"))
+  (doall (map #(append ($ :#results) (str "<li>" % "</li>")) results)))
+
+(defn perform-search [query]
+  (js/jQuery.getJSON (str "http://api.flickr.com/services/rest/?method=flickr.groups.search&api_key=e400c83e08716edc21ce04d19a71d697&text=" query "&per_page=10&format=json&jsoncallback=?")))
+
+(defn group-names [response]
+  (if-let [groups (aget response "groups")]
+    (map #(aget % "name") (aget groups "group"))))
+
+(let [changes   (event-list query-input "change")
+      keyups    (event-list query-input "keyup")
+      c-events  (concat* changes keyups)
+      changes   (mapd* summarise c-events)
+      queries   (mapd* :value changes)
+      responses (map* perform-search queries)
+      groups    (mapd* group-names responses)]
+  (mapd* update-query-title queries)
+  (mapd* update-results-list groups))
 
