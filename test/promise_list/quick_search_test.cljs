00abc74601b81993c4953ccf2f1869533b649517
Logan Campbell <colin@logaan.net>
2013-07-02 01:47:41 +1000

    tidied up quick search

--- test/promise_list/quick_search_test.cljs
+++ test/promise_list/quick_search_test.cljs
@@ -4,6 +4,11 @@
         [promise-list.pcell :only [deferred]]
         [promise-list.plist :only [with-open-plist append! map* mapd* concat*]]))
 
+(defn event-list [element event-type]
+  (with-open-plist (fn [writer]
+    (on element event-type (fn [event]
+      (append! writer (deferred event)))))))
+
 (defn summarise [event]
   (let [target (aget event "target")]
     {:type   (aget event "type")
@@ -11,24 +16,9 @@
      :target target
      :value  (aget target "value")}))
 
-(defn event-list [element event-type]
-  (with-open-plist (fn [writer]
-    (on element event-type (fn [event]
-      (append! writer (deferred event)))))))
-
 (defn transparent-log [v]
   (log (clj->js v))
   v)
-
-(def query-input ($ :#query))
-(def query-title ($ :#query-title))
-
-(defn update-query-title [new-title]
-  (text query-title new-title))
-
-(defn update-results-list [results]
-  (remove ($ "#results li"))
-  (doall (map #(append ($ :#results) (str "<li>" % "</li>")) results)))
 
 (defn perform-search [query]
   (js/jQuery.getJSON (str "http://api.flickr.com/services/rest/?method=flickr.groups.search&api_key=e400c83e08716edc21ce04d19a71d697&text=" query "&per_page=10&format=json&jsoncallback=?")))
@@ -37,13 +27,16 @@
   (if-let [groups (aget response "groups")]
     (map #(aget % "name") (aget groups "group"))))
 
-(let [changes   (event-list query-input "change")
-      keyups    (event-list query-input "keyup")
-      c-events  (concat* changes keyups)
-      changes   (mapd* summarise c-events)
-      queries   (mapd* :value changes)
-      responses (map* perform-search queries)
+(defn update-results-list [results]
+  (remove ($ "#results li"))
+  (mapv #(append ($ :#results) (str "<li>" % "</li>")) results))
+
+(let [changes   (event-list ($ :#query) "change")
+      keyups    (event-list ($ :#query) "keyup")
+      events    (concat* changes keyups)
+      queries   (mapd* (comp :value summarise) events)
+      responses (map*  perform-search queries)
       groups    (mapd* group-names responses)]
-  (mapd* update-query-title queries)
+  (mapd* (partial text ($ :#query-title)) queries)
   (mapd* update-results-list groups))
 
