2601486ca9ff6c6224d919bf6d00866e719fb9dc
Logan Campbell <colin@logaan.net>
2013-08-02 21:52:07 +1000

    searching from local endpoint and filtering queries > 3

--- test/promise_stream/quick_search_example.cljs
+++ test/promise_stream/quick_search_example.cljs
@@ -1,45 +1,32 @@
 (ns promise-stream.quick-search-test
   (:require [jayq.core :as jq]) 
-  (:use [jayq.core            :only [$]]
-        [promise-stream.sources :only [event-stream]]
-        [promise-stream.pstream   :only [mapd* concat* throttle*]]
-        [promise-stream.timing-aware :only
-         [resolve-order-map* keep-most-recently-requested
-          stamp-with-request-time]]))
+  (:use [jayq.core                   :only [$]]
+        [promise-stream.sources      :only [event-stream]]
+        [promise-stream.pstream      :only [mapd* concat* filter* throttle* promise]]
+        [promise-stream.timing-aware :only [resolve-order-map*
+                                            keep-most-recently-requested
+                                            stamp-with-request-time]]))
 
 (defn perform-search [query]
-  (js/jQuery.getJSON
-    (str "http://api.flickr.com/services/rest/"
-         "?method="       "flickr.groups.search"
-         "&api_key="      "10b278da620908b32d4cb5e044366699"
-         "&text="         query
-         "&per_page="     10
-         "&format="       "json"
-         "&jsoncallback=" "?")))
-
-(defn group-names [response]
-  (if-let [groups (aget response "groups")]
-    (for [group (aget groups "group")]
-      (aget group "name"))))
+  (js/jQuery.get (str "/dictionary?search=" query)))
 
 (defn set-query-title! [new-title]
   (jq/text ($ :#query-title) new-title))
 
-(defn set-results-stream! [results]
-  (jq/remove ($ "#results li"))
-  (mapv #(jq/append ($ :#results) (str "<li>" % "</li>")) results))
+(defn set-results! [words]
+  (jq/text ($ :#results) words))
 
 ((fn []
   (let [changes   (event-stream ($ :#query) "change")
         keyups    (event-stream ($ :#query) "keyup")
         events    (throttle* 400 (concat* changes keyups))
         queries   (mapd* #(.-value (.-target %)) events)
+        long-queries (filter* (comp promise #(< 3 (count %))) queries)
         responses (resolve-order-map* 
-                    (stamp-with-request-time perform-search) queries)
-        filtered  (keep-most-recently-requested responses)
-        groups    (mapd* group-names filtered)]
-    (mapd* set-query-title!  queries)
-    (mapd* set-results-stream! groups))))
+                    (stamp-with-request-time perform-search) long-queries)
+        words     (keep-most-recently-requested responses) ]
+    (mapd* set-query-title! long-queries)
+    (mapd* set-results! words))))
 
 (defn update-latest-result [response]
   (jq/text ($ :#latest_result) response))
