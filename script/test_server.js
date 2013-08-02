b4b78474224239a9ec3e75147de0c038dd8a848a
Logan Campbell <colin@logaan.net>
2013-08-02 21:22:14 +1000

    added endpoint to search local dictionary

--- script/test_server.js
+++ script/test_server.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 var http = require('http'),
-    fs   = require('fs');
+    fs   = require('fs'),
+    url  = require('url'),
+    qs   = require('querystring'),
+    exec = require('child_process').exec;
 
 http.createServer(function (req, res) {
 
-  switch(req.url) {
+  switch(url.parse(req.url, true).pathname) {
     case "/":
       res.writeHead(200, {'Content-Type': 'text/html'});
       fs.readFile('resources/public/index.html', function(err, data) {
@@ -13,16 +16,30 @@ http.createServer(function (req, res) {
         res.end(data);
       });
       break;
+
     case "/slow":
       setTimeout(function() {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('slow');
       }, 1000);
       break;
+
     case "/fast":
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('fast');
       break;
+
+    case "/dictionary":
+      var query = url.parse(req.url, true).query.search;
+      var wordsFile = "/usr/share/dict/words";
+
+      exec("grep '" + query + "' " + wordsFile, function (error, stdout) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(stdout);
+      });
+
+      break;
+
     default:
       fs.readFile('resources/public' + req.url, function(err, data) {
         if (err) console.log(err);
