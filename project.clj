ff9564bcb29603d0685333ad451d6b5b27a773f7
Logan Campbell <colin@logaan.net>
2013-08-02 21:58:55 +1000

    tidied up project for 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject promise-stream "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject promise-stream "0.1.0"
+  :description "A promise stream serves the same purpose as a blocking lazy
+               sequence. Javascript code may not block and so an asynchronous
+               alternative is required."
+  :url "https://github.com/logaan/promise-stream"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
